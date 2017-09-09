40295bb804e4a9176d7ec5c279e85c0685b6e011
Praiya Jupiter <praiya_kung@hotmail.com>
2017-09-09 13:02:10 +0700

    Add project files.

---
+++ WebIOTDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebIOTDemo.Global" Language="C#" %>
---
+++ WebIOTDemo/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebIOTDemo.Startup))]
+namespace WebIOTDemo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
